0c8c5f73fe75784cc6f1ead56476c5f1ef93663b
石黒みなみ <ishigurominami142@gmail.com>
2024-11-14 14:31:34 +0900

    11/14修正

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -53,18 +53,18 @@ $("#q8").on({mouseenter: function(){
 //q9内のリスト項目ををクリックしたときに
 $("#q9 li").on("click",function(){
   // クリックされたリスト項目のインデックス番号（配列的な位置）を取得する
-  const q9List = $(this).index();
+  const selectedIndex = $(this).index();
   //配列の番号をアラート表示させる
-  alert(q9List);
+  alert(selectedIndex);
 });
 
 //q10内のリスト項目（li）がクリックされたときに
 $("#q10 li").on("click",function(){
-  //クリックされたq10のリスト項目のインデックス番号を取得し、変数q10Listに代入する
-  const q10List = $(this).index();
-  //q11内の、インデックス番号q10Listに該当するリスト項目（li）をコンソールに表示する
-  console.log($("#q11 li").eq(q10List));
-  //インデックス番号q10Listに該当するq11のリスト項目に、"large-text"というクラスを追加し、文字を大きくする
-  $("#q11 li").eq(q10List).addClass("large-text")
+  //クリックされたq10のリスト項目のインデックス番号を取得し、変数itemIndexに代入する
+  const itemIndex = $(this).index();
+  //q11内の、インデックス番号itemIndexに該当するリスト項目（li）をコンソールに表示する
+  console.log($("#q11 li").eq(itemIndex));
+  //インデックス番号itemIndexに該当するq11のリスト項目に、"large-text"というクラスを追加し、文字を大きくする
+  $("#q11 li").eq(itemIndex).addClass("large-text")
 })
 });
--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -4,22 +4,23 @@ $(function () {
     //選択されたドロップダウンリストの値を取得し、変数 dropdownlist に格納、
     // また、変数dropdownListには、クラスが "food-list" に割り当てられた要素のリストアイテムが格納します。
     const dropdownList = $(this).val(), foodList = $(".food-list li");
-    // 変数 dropdownlist の値が "all" である場合は、全てのリストアイテムを表示
+    // 変数 dropdownlist の値が "all" である場合は、
     if (dropdownList === "all") {
-      c.show();
+      //全てのリストアイテムを表示
+      foodList.show();
     } else {
       // それ以外の場合はリストアイテムを個別に処理
       // 各リストアイテムのデータ属性と foodList の値を比較して一致する場合は表示、一致しない場合は非表示にする
-      $.each(foodList, function (e, a) {
+      $.each(foodList, function (index, listItem) {
         // リストアイテムのデータ属性 "category-type" の値を取得し、変数 listcategory に格納
-        const listCategory = $(a).data("category-type");
+        const listCategory = $(listItem).data("category-type");
         // dropdownlist の値と listcategory の値を比較
         if (dropdownList === listCategory) {
           // 一致する場合はリストアイテムを表示
-          $(a).show();
+          $(listItem).show();
         } else {
           // 一致しない場合は非表示にする
-          $(a).hide();
+          $(listItem).hide();
         }
       });
     }
